69138d3968ff1c24d59603f068333f3faa9b046d
Max Große <max12.gro@gmail.com>
2024-07-18 22:20:25 +0200

    press play

---
+++ lib/spotify.ml
@@ -0,0 +1,309 @@
+module Config = struct
+  type t = { client_id : string; client_secret : string }
+
+  let make ~client_id ~client_secret = { client_id; client_secret }
+  let client_id ~config = config.client_id
+
+  let request_token ~config =
+    Base64.encode_exn (config.client_id ^ ":" ^ config.client_secret)
+end
+
+module User = struct
+  type t = {
+    access_token : string;
+    scope : string;
+    expires_at : int; (* Unix timestamp *)
+    refresh_token : string;
+  }
+  [@@deriving serialize, deserialize]
+
+  let of_auth_response ~access_token ~refresh_token ~expires_in ~scope =
+    {
+      access_token;
+      refresh_token;
+      expires_at = expires_in + int_of_float (Unix.time ());
+      scope;
+    }
+
+  let to_string ~user =
+    let user =
+      {
+        access_token = user.access_token;
+        scope = user.scope;
+        expires_at = user.expires_at;
+        refresh_token = user.refresh_token;
+      }
+    in
+    Serde_json.to_string serialize_t user
+
+  let of_string ~user =
+    let user =
+      Format.eprintf "trying to ge a user\n";
+      Serde_json.of_string deserialize_t user
+    in
+    Format.eprintf "we got a user.. maybe?\n";
+    user
+
+  let access_token ~user = user.access_token
+  let refresh_token ~user = user.refresh_token
+
+  let is_valid ~user =
+    Format.eprintf "is valid?\n";
+    user.expires_at >= int_of_float (Unix.time ()) + 3000
+end
+
+module Client = struct
+  type t = { config : Config.t }
+
+  type server_auth_response = {
+    access_token : string;
+    token_type : string;
+    expires_in : int;
+  }
+  [@@deriving serialize, deserialize]
+
+  type user_auth_response = {
+    access_token : string;
+    token_type : string;
+    scope : string;
+    expires_in : int;
+    refresh_token : string;
+    error : string option;
+    error_description : string option;
+  }
+  [@@deriving serialize, deserialize]
+
+  type auth_response =
+    | User of user_auth_response
+    | Server of server_auth_response
+  [@@deriving serialize, deserialize]
+
+  let auth_url = Uri.of_string "https://accounts.spotify.com/authorize"
+  let token_url = Uri.of_string "https://accounts.spotify.com/api/token"
+  let make ~config = { config }
+  let make_empty = { config = { client_id = ""; client_secret = "" } }
+
+  let redirect_uri ~client ~scope ~redirect_uri ~state =
+    Uri.add_query_params auth_url
+      [
+        ("response_type", [ "code" ]);
+        ("client_id", [ Config.client_id ~config:client.config ]);
+        ("scope", [ scope ]);
+        ("redirect_uri", [ redirect_uri ]);
+        ("state", [ state ]);
+      ]
+
+  let ph name value header = Cohttp.Header.add header name value
+
+  let print_response print (resp, body) =
+    let open Cohttp in
+    let open Lwt in
+    if print then (
+      let code = resp |> Response.status |> Code.code_of_status in
+      Printf.printf "Response code: %d\n" code;
+      Printf.printf "Headers: %s\n"
+        (resp |> Response.headers |> Header.to_string);
+      body |> Cohttp_lwt.Body.to_string >|= fun body ->
+      Printf.printf "Body: %s\n" body;
+      body)
+    else body |> Cohttp_lwt.Body.to_string >|= fun body -> body
+
+  let login_as_user ~client ~auth_code ~uri =
+    Format.eprintf "logging in as user...\n";
+    let request_token = Config.request_token ~config:client.config in
+    let headers =
+      Cohttp.Header.init () |> ph "Authorization" ("Basic " ^ request_token)
+    in
+    let params =
+      [
+        ("grant_type", [ "authorization_code" ]);
+        ("code", [ auth_code ]);
+        ("redirect_uri", [ uri ]);
+      ]
+    in
+    let open Lwt in
+    let%lwt resp =
+      Cohttp_lwt_unix.Client.post_form ~headers ~params token_url
+      >>= print_response false
+    in
+    Lwt.return
+      (Serde_json.of_string deserialize_user_auth_response resp
+      |> Result.map (fun user ->
+             User.of_auth_response ~access_token:user.access_token
+               ~refresh_token:user.refresh_token ~scope:user.scope
+               ~expires_in:user.expires_in))
+
+  let get ~user ~url =
+    let open Lwt in
+    let headers =
+      Cohttp.Header.init ()
+      |> ph "Authorization" ("Bearer " ^ User.access_token ~user)
+    in
+    let req =
+      Cohttp_lwt_unix.Client.get ~headers (Uri.of_string url)
+      >>= print_response false
+    in
+    req
+
+  let refresh_user ~client ~user : (User.t, Serde.error) result Lwt.t =
+    let request_token = Config.request_token ~config:client.config in
+    let headers =
+      Cohttp.Header.init () |> ph "Authorization" ("Basic " ^ request_token)
+    in
+    let params =
+      [
+        ("grant_type", [ "refresh_token" ]);
+        ("refresh_token", [ User.refresh_token ~user ]);
+      ]
+    in
+    let open Lwt in
+    let%lwt resp =
+      Cohttp_lwt_unix.Client.post_form ~headers ~params token_url
+      >>= print_response false
+    in
+    let user = User.of_string ~user:resp in
+    Lwt.return user
+end
+
+module Api = struct
+  type imageObject = { url : string; height : int option; width : int option }
+  [@@deriving deserialize]
+
+  type externalUrl = { spotify : string } [@@deriving deserialize]
+
+  type followers = { href : string option; total : int }
+  [@@deriving deserialize]
+
+  type restrictions = { reason : string } [@@deriving deserialize]
+
+  type simplifiedArtist = {
+    external_urls : externalUrl;
+    href : string;
+    id : string;
+    name : string;
+    item_type : string; [@serde { rename = "type" }]
+    uri : string;
+  }
+  [@@deriving deserialize]
+
+  type album = {
+    album_type : string;
+    total_tracks : int;
+    available_markets : string list;
+    external_urls : externalUrl;
+    href : string;
+    id : string;
+    images : imageObject list;
+    name : string;
+    release_date : string;
+    release_date_precision : string;
+    restrictions : restrictions option;
+    item_type : string; [@serde { rename = "type" }]
+    uri : string;
+    artists : simplifiedArtist list;
+  }
+  [@@deriving deserialize]
+
+  type artist = {
+    external_urls : externalUrl;
+    followers : followers option;
+    genres : string list option;
+    href : string;
+    id : string;
+    images : imageObject list option;
+    name : string;
+    popularity : int option;
+    item_type : string; [@serde { rename = "type" }]
+    uri : string;
+  }
+  [@@deriving deserialize]
+
+  type external_ids = {
+    isrc : string;
+    ean : string option;
+    upc : string option;
+  }
+  [@@deriving deserialize]
+
+  (* We don't know what this looks like *)
+  type linked_from = { anything : string option } [@@deriving deserialize]
+
+  type track = {
+    album : album;
+    artists : artist list;
+    available_markets : string list;
+    disc_number : int;
+    duration_ms : int;
+    explicit : bool;
+    external_ids : external_ids;
+    external_urls : externalUrl;
+    href : string;
+    id : string;
+    is_playable : bool option;
+    linked_from : linked_from option;
+    restrictions : restrictions option;
+    name : string;
+    popularity : int;
+    preview_url : string option;
+    track_number : int;
+    track_type : string; [@serde { rename = "type" }]
+    uri : string;
+    is_local : bool;
+  }
+  [@@deriving deserialize]
+
+  type userTopTracksResponse = {
+    href : string;
+    limit : int;
+    next : string option;
+    offset : int;
+    previous : string option;
+    total : int;
+    items : track list;
+  }
+  [@@deriving deserialize]
+
+  type userTopArtistsResponse = {
+    href : string;
+    limit : int;
+    next : string option;
+    offset : int;
+    previous : string option;
+    total : int;
+    items : artist list;
+  }
+  [@@deriving deserialize]
+
+  let base_url = "https://api.spotify.com/v1"
+
+  let get_tracks ~user ~url :
+      (userTopTracksResponse * User.t, Serde.error) result Lwt.t =
+    Format.eprintf "getting tracks for user: \n";
+    let%lwt resp = Client.get ~user ~url in
+    let maybe_tracks =
+      Serde_json.of_string deserialize_userTopTracksResponse resp
+    in
+    Lwt.return
+      (match maybe_tracks with
+      | Ok tracks -> Ok (tracks, user)
+      | Error e ->
+          Format.eprintf "can't get user top tracks response: %s\n" resp;
+          Serde.pp_err Format.err_formatter e;
+          Error e)
+
+  let user_top_tracks ~client ~user =
+    Format.eprintf "getting user's top tracks...\n";
+    let%lwt user =
+      if User.is_valid ~user then Lwt.return (Ok user)
+      else Client.refresh_user ~client ~user
+    in
+    match user with
+    | Error e ->
+        Format.eprintf "Can't get user: n";
+        Serde.pp_err Format.err_formatter e;
+        Lwt.return (Error e)
+    | Ok user ->
+        let url = base_url ^ "/me/top/tracks" in
+        let tracks = get_tracks ~url ~user in
+        tracks
+end
---
+++ lib/spotify.mli
@@ -0,0 +1,151 @@
+module Config : sig
+  type t
+
+  val make : client_id:string -> client_secret:string -> t
+  val client_id : config:t -> string
+
+  val request_token : config:t -> string
+  (** Returns client_id:client_secret base64 encoded. *)
+end
+
+module User : sig
+  type t
+
+  val to_string : user:t -> (string, Serde.error) result
+  val of_string : user:string -> (t, Serde.error) result
+
+  val of_auth_response :
+    access_token:string ->
+    refresh_token:string ->
+    expires_in:int ->
+    scope:string ->
+    t
+
+  val access_token : user:t -> string
+  val refresh_token : user:t -> string
+  val is_valid : user:t -> bool
+end
+
+module Client : sig
+  type t
+
+  val make : config:Config.t -> t
+  val make_empty : t
+
+  val redirect_uri :
+    client:t -> scope:string -> redirect_uri:string -> state:string -> Uri.t
+
+  val login_as_user :
+    client:t ->
+    auth_code:string ->
+    uri:string ->
+    (User.t, Serde.error) result Lwt.t
+
+  val refresh_user :
+    client:t -> user:User.t -> (User.t, Serde.error) result Lwt.t
+
+  val get : user:User.t -> url:string -> string Lwt.t
+end
+
+module Api : sig
+  type imageObject = { url : string; height : int option; width : int option }
+  type externalUrl = { spotify : string }
+  type followers = { href : string option; total : int }
+  type restrictions = { reason : string }
+
+  type simplifiedArtist = {
+    external_urls : externalUrl;
+    href : string;
+    id : string;
+    name : string;
+    item_type : string;
+    uri : string;
+  }
+
+  type album = {
+    album_type : string;
+    total_tracks : int;
+    available_markets : string list;
+    external_urls : externalUrl;
+    href : string;
+    id : string;
+    images : imageObject list;
+    name : string;
+    release_date : string;
+    release_date_precision : string;
+    restrictions : restrictions option;
+    item_type : string;
+    uri : string;
+    artists : simplifiedArtist list;
+  }
+
+  type artist = {
+    external_urls : externalUrl;
+    followers : followers option;
+    genres : string list option;
+    href : string;
+    id : string;
+    images : imageObject list option;
+    name : string;
+    popularity : int option;
+    item_type : string;
+    uri : string;
+  }
+
+  type external_ids = {
+    isrc : string;
+    ean : string option;
+    upc : string option;
+  }
+
+  type linked_from = { anything : string option }
+
+  type track = {
+    album : album;
+    artists : artist list;
+    available_markets : string list;
+    disc_number : int;
+    duration_ms : int;
+    explicit : bool;
+    external_ids : external_ids;
+    external_urls : externalUrl;
+    href : string;
+    id : string;
+    is_playable : bool option;
+    linked_from : linked_from option;
+    restrictions : restrictions option;
+    name : string;
+    popularity : int;
+    preview_url : string option;
+    track_number : int;
+    track_type : string;
+    uri : string;
+    is_local : bool;
+  }
+
+  type userTopTracksResponse = {
+    href : string;
+    limit : int;
+    next : string option;
+    offset : int;
+    previous : string option;
+    total : int;
+    items : track list;
+  }
+
+  type userTopArtistsResponse = {
+    href : string;
+    limit : int;
+    next : string option;
+    offset : int;
+    previous : string option;
+    total : int;
+    items : artist list;
+  }
+
+  val user_top_tracks :
+    client:Client.t ->
+    user:User.t ->
+    (userTopTracksResponse * User.t, Serde.error) result Lwt.t
+  (** Returns the users' top tracks and also an updated User object. *)
+end
